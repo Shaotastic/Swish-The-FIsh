79e5e3c59fdc56f9986de43084cdac0a2e5305d7
Shaotastic <aaronmac13@hotmail.com>
2021-01-07 13:02:02 -0500

    Basic

---
+++ Assets/_Scripts/Emotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PositiveEmotion
+{ 
+    Happy,
+}
+
+public enum NegativeEmotion
+{
+    Sad,
+}
+
+public enum Emotions
+{
+    Happy,
+    Sad
+}
+
+[CreateAssetMenu(fileName = "New Emotion", menuName = "Emotions")]
+public class Emotion : ScriptableObject
+{
+    public Emotions mainEmotion;
+
+    public List<Emotions> oppositeEmotions;
+
+    public Sprite sprite;
+}
---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        if(h > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+            spriteRenderer.flipX = false;
+
+        transform.position += new Vector3(h * Time.deltaTime * speed, v * Time.deltaTime * speed, 0);
+    }
+}
